1f032528be18e580d496828a01512d17b1002d91
Dave Arnold <darnold@l-sfh3k1htdd-m.gapinc.gap.com>
2018-05-11 11:52:14 -0700

    updating

--- team.tf
+++ team.tf
@@ -1,6 +1,5 @@
 # Add a team to the organization
 resource "github_team" "team" {
-  count       = "${var.setup ? 1 : 0}"
   name        = "${var.team}"
   description = "${var.description}"
   privacy     = "${var.privacy}"
